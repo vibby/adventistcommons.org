614235ac2ae82785473dcf6a175545ec02c7032f
Adam Jackson <akjackson1@gmail.com>
2019-06-25 08:11:51 -0700

    Initial database queries for email digest

---
+++ application/controllers/Cron.php
@@ -0,0 +1,97 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cron extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->model( "project_model" );
+	}
+	
+	public function send_email_digest()
+	{
+		$time = ( new DateTime() )->modify( "-1 day" )->format( "Y-m-d G:i:s" );
+		
+		$users = $this->db->select( "*" )
+			->from( "users" )
+			->where( "digest_email_processed_at <=", $time )
+			->or_where( "digest_email_processed_at =", null )
+			->limit( 50 )
+			->get()
+			->result_array();
+		
+		foreach( $users as $user ) {
+			$projects = [];
+			$last_processed = $user["digest_email_processed_at"] ?? $time;
+			$revised_content = $this->_activityQuery( "product_content_revisions", $last_processed, $user["id"] );
+			$activity = $this->_activityQuery( "product_content_log", $last_processed, $user["id"] );
+			
+			if( ! $revised_content ) {
+				//digest_email_processed_at timestamp
+				continue;
+			}
+			
+			foreach( $revised_content as $content ) {
+				$project_id = $content["project_id"];
+				$content_id = $content["content_id"];
+				$user_id = $content["user_id"];
+				
+				if( ! isset( $projects[$project_id] ) ) {
+					$projects[$project_id]["name"] = $content["project_name"];
+				}
+				if( ! isset( $projects[$project_id]["user_revisions"][$user_id] ) ) {
+					$projects[$project_id]["user_revisions"][$user_id] = [
+						"name" => $content["user_name"],
+						"revisions" => 1,
+					];
+				} else {
+					$projects[$project_id]["user_revisions"][$user_id]["revisions"]++;
+				}
+			}
+			
+			foreach( $activity as $content ) {
+				$project_id = $content["project_id"];
+				$content_id = $content["content_id"];
+				$user_id = $content["user_id"];
+				$type = $content["type"];
+				
+				if( ! isset( $projects[$project_id] ) ) {
+					$projects[$project_id]["name"] = $content["project_name"];
+				}
+				if( ! isset( $projects[$project_id]["user_revisions"][$user_id] ) ) {
+					$projects[$project_id]["user_revisions"][$user_id]["name"] = $content["user_name"];
+				}
+				if( ! isset( $projects[$project_id]["user_revisions"][$user_id][$type] ) ) {
+					$projects[$project_id]["user_revisions"][$user_id][$type] = 1;
+				} else {
+					$projects[$project_id]["user_revisions"][$user_id][$type]++;
+				}
+			}
+			
+			print_r($projects);die;
+		}
+	}
+	
+	private function _activityQuery( $table, $date, $user_id ) {
+		$group_by = [ "user_id", "content_id", "section_id", "project_id" ];
+		$extra_select = "";
+		if( $table == "product_content_log" ) {
+			$extra_select = "activity_table.type as type";
+			array_push( $group_by, "type" );
+		}
+		return $this->db->select( "activity_table.content_id, activity_table.user_id, activity_table.project_id, CONCAT(products.name, ' (', languages.name, ')') as project_name, CONCAT(users.first_name, ' ', users.last_name) as user_name, $extra_select" )
+			->from( "$table as activity_table" )
+			->group_by( $group_by )
+			->join( "product_content", "activity_table.content_id = product_content.id" )
+			->join( "products", "product_content.product_id = products.id" )
+			->join( "projects", "activity_table.project_id = projects.id" )
+			->join( "languages", "projects.language_id = languages.id" )
+			->join( "users", "activity_table.user_id = users.id" )
+			->where( "created_at >=", $date )
+			->where( "user_id", $user_id )
+			->get()
+			->result_array();
+	}
+}
