2da896b6e00df12dd474369cbe13d0921ab2c5bc
Adam Jackson <akjackson1@gmail.com>
2019-06-26 12:29:09 -0700

    Renaem Cron ontroller to Cli

--- application/controllers/Cli.php
+++ application/controllers/Cli.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Cron extends CI_Controller {
+class Cli extends CI_Controller {
 	
 	public function __construct()
 	{
