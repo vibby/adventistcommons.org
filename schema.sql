0632a54acef9d1fbba35b4f0bcdc69b57d940f45
Sergey B <svb.code@gmail.com>
2019-10-16 17:18:33 +0300

    #0po4EKCy Updated products db-entity

--- schema.sql
+++ schema.sql
@@ -264,7 +264,6 @@ CREATE TABLE `products` (
   `page_count` int(4) DEFAULT NULL,
   `type` enum('book','booklet','magabook','tract') DEFAULT 'book',
   `idml_file` varchar(255) DEFAULT NULL,
-  `audience` varchar(255) DEFAULT NULL,
   `publisher` varchar(255) DEFAULT NULL,
   `format_open` varchar(32) DEFAULT NULL,
   `format_closed` varchar(32) DEFAULT NULL,
